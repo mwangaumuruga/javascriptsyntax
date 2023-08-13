9b8d3988ccc1041a31d215c5a81ad27504631d03
codekartel <codekartel@localhost.localdomain>
2023-08-13 08:20:24 +0300

    to do list edited

--- dailyjstasks/day3/simpletodolist.js
+++ dailyjstasks/day3/simpletodolist.js
@@ -1,3 +1,25 @@
-function(){
-    
-}
+const taskForm = document.getElementById("taskForm");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+// const editTask=docment.getElementById("taskList");
+
+taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const listItem = document.createElement("li");
+        listItem.textContent = taskText;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete Task";
+        deleteButton.addEventListener("click", function () {
+            taskList.removeChild(listItem);
+        });
+
+        listItem.appendChild(deleteButton);
+        taskList.appendChild(listItem);
+
+        taskInput.value = "";
+    }
+});
